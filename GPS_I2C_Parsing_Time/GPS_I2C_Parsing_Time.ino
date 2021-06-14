20de535a8d6072d1b9f0efd74c561b0379676691
Nathan <nathanael.esnault@gmail.com>
2021-06-15 00:15:23 +1200

    Update GPS_I2C_Parsing_Time.ino

--- GPS_I2C_Parsing_Time/GPS_I2C_Parsing_Time.ino
+++ GPS_I2C_Parsing_Time/GPS_I2C_Parsing_Time.ino
@@ -40,6 +40,7 @@ Adafruit_GPS GPS(&Wire);
 #define GPSECHO false
 
 uint32_t lastTime = millis(); // Just for display <REMOVE>
+uint32_t timer = millis();// Just for display <REMOVE>
 
 // My variables
 char    timeStampFormat_Line[]      = "YYYY_MM_DD__hh_mm_ss";   // naming convention for EACH LINE OF THE FILE logged to the SD card
@@ -65,12 +66,13 @@ void setup()
   // Turn OFF all GPS output
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_OFF);
 
-//  GPS.flush();
+  //  GPS.flush();
 
   // Set up communication
   // ---------------------
   GPS.sendCommand(PMTK_SET_BAUD_115200); // Ask the GPS to send us data as 115200
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ); // 10 Hz update rate (message only)
+  //GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
   GPS.sendCommand(PMTK_API_SET_FIX_CTL_5HZ);  // Can't fix position faster than 5 times a second
 
 
@@ -78,26 +80,26 @@ void setup()
   // Just once, request updates on antenna status, comment out to keep quiet
   GPS.sendCommand(PGCMD_ANTENNA);
   delay(1000);
-  #ifdef SERIAL_VERBOSE
-    while (GPS.available())
-    {
-      Serial.print(GPS.read());
-    }
-  #endif
+  //  #ifdef SERIAL_VERBOSE
+  //    while (GPS.available())
+  //    {
+  //      Serial.print(GPS.read());
+  //    }
+  //  #endif
 
   //  Just once, request firmware version
   GPS.println(PMTK_Q_RELEASE);
   delay(1000);
-    #ifdef SERIAL_VERBOSE
-    while (GPS.available())
-    {
-      Serial.print(GPS.read());
-    }
-  #endif
+  //    #ifdef SERIAL_VERBOSE
+  //    while (GPS.available())
+  //    {
+  //      Serial.print(GPS.read());
+  //    }
+  //  #endif
 
   // Turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  //  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
 
 
 }
@@ -106,132 +108,73 @@ void setup()
 // -------------------------- Loop --------------------------
 void loop() // run over and over again
 {
-
-    char c = GPS.read();
-    // if you want to debug, this is a good time to do it!
-    
-    if (c) 
-    {
-      Serial.print(c);
-    }
+  // read data from the GPS in the 'main loop'
+  char c = GPS.read();
 
   // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) 
-  {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    #ifdef SERIAL_VERBOSE
-      Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    #endif
-    
+  if (GPS.newNMEAreceived()) {
+
+
     if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+    return; // we can fail to parse a sentence in which case we should just wait for another
+
+    if(not(GPS.fix))
     {
-      return; // we can fail to parse a sentence in which case we should just wait for another
+      Serial.print("Fix: ");
+      Serial.print((int)GPS.fix);
+      Serial.print(" quality: ");
+      Serial.println((int)GPS.fixquality);
     }
-    
-  }
-
-  // approximately every 2 seconds or so, print out the current stats
-  if (millis() - lastTime > 2000) 
-  {
-    lastTime = millis(); // reset the timer
-
-    //Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    Serial.print("Fix: "); Serial.println((int)GPS.fix);
-
-    #ifdef SERIAL_VERBOSE
-    if (GPS.fix) 
+    else
     {
-      Serial.println("We have a fix");
-    }
+      // My time formating
+      //------------------
+      // Create a string for assembling the data to log
+      String dataString  = "";
+      String myTimestamp = "";
 
-    Serial.print("Datetime: ");
+      // Add start of message charachter
+      //--------------------------------
+      dataString += SOM_LOG;
 
-    Serial.print("20");
-    if (GPS.year < 10) { Serial.print('0'); }
-    Serial.print(GPS.year, DEC);
-    Serial.print("_");
-    if (GPS.month < 10) { Serial.print('0'); }
-    Serial.print(GPS.month, DEC);
-    Serial.print("_");
-    if (GPS.day < 10) { Serial.print('0'); }
-    Serial.print(GPS.day, DEC);
-    Serial.print("__"); 
-    
-   
-    if (GPS.hour < 10) { Serial.print('0'); }
-    Serial.print(GPS.hour, DEC); 
-    Serial.print('_');
-    
-    if (GPS.minute < 10) { Serial.print('0'); }
-    Serial.print(GPS.minute, DEC);
-    Serial.print('_');
-    
-    if (GPS.seconds < 10) { Serial.print('0'); }
-    Serial.print(GPS.seconds, DEC); 
-    Serial.print('.');
-    
-    if (GPS.milliseconds < 10) 
-    {
-      Serial.print("00");
-    } 
-    else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) 
-    {
-      Serial.print("0");
-    }
-    Serial.println(GPS.milliseconds);
-    #endif
+      // Read the GPS time
+      //------------------------------------------
+      
+      dataString += String(2000 + GPS.year);
+      dataString += "_";
+      if (GPS.month < 10) { dataString += "0"; } 
+      dataString += String(GPS.month);
+      dataString += "_";
+      if (GPS.day < 10) { dataString += "0"; } 
+      dataString += String(GPS.day);
+      dataString += "__";
+      if (GPS.hour < 10) { dataString += "0"; } 
+      dataString += String(GPS.hour);
+      dataString += "_";
+      if (GPS.hour < 10) { dataString += "0"; } 
+      dataString += String(GPS.minute);
+      dataString += "_";
+      if (GPS.hour < 10) { dataString += "0"; } 
+      dataString += String(GPS.seconds);
+      dataString += ".";
 
-
-    // My time formating
-    //----------------------------------------------------------------------------------
-    // Create a string for assembling the data to log
-    String dataString  = "";
-    String myTimestamp = "";
-  
-    // Add start of message charachter
-    //--------------------------------
-    dataString += SOM_LOG;
-  
-    // Read the GPS time
-    //------------------------------------------
-    
-    dataString += String(2000 + GPS.year);
-    dataString += "_";
-    if (GPS.month < 10) { dataString += "0"; } 
-    dataString += String(GPS.month);
-    dataString += "_";
-    if (GPS.day < 10) { dataString += "0"; } 
-    dataString += String(GPS.day);
-    dataString += "__";
-    if (GPS.hour < 10) { dataString += "0"; } 
-    dataString += String(GPS.hour);
-    dataString += "_";
-    if (GPS.hour < 10) { dataString += "0"; } 
-    dataString += String(GPS.minute);
-    dataString += "_";
-    if (GPS.hour < 10) { dataString += "0"; } 
-    dataString += String(GPS.seconds);
-    dataString += ".";
-
-    if (GPS.milliseconds < 10) 
-    {
-      dataString += "00";
-    } 
-    else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) 
-    {
-      dataString += "0";
-    }
-    dataString += String(GPS.milliseconds);
+//      if (GPS.milliseconds < 10) 
+//      {
+//        dataString += "00";
+//      } 
+//      else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) 
+//      {
+//        dataString += "0";
+//      }
+      dataString += String(GPS.milliseconds);
 
 
-    dataString += FORMAT_SEP;
+      dataString += FORMAT_SEP;
 
-    #ifdef SERIAL_VERBOSE
+      #ifdef SERIAL_VERBOSE
       Serial.print("Timestamp: ");
       Serial.println(dataString);
-    #endif
-
+      #endif
+    }
   }
 }
