b464f5fc87e0bf50230f7ce703da967e40242871
Nathan <nathanael.esnault@gmail.com>
2021-07-26 16:38:21 +1200

    Putting stuff in .h files

---
+++ Final-Version/RS1D.h
@@ -0,0 +1,33 @@
+/* ========================================
+*
+* Copyright University of Auckland Ltd, 2021
+* All Rights Reserved
+* UNPUBLISHED, LICENSED SOFTWARE.
+*
+* Metadata
+* Written by    : Nathanaël Esnault
+* Verified by   : Nathanaël Esnault
+* Creation date : 2021-07-26
+* Version       : 0.1 (finished on 2021-..-..)
+* Modifications :
+* Known bugs    :
+*
+*
+* Possible Improvements
+*
+* Notes
+*
+*
+* Ressources (Boards + Libraries Manager)
+*
+*
+* TODO
+*
+* ========================================
+*/
+
+
+
+
+
+// END OF THE FILE
