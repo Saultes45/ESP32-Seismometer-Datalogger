dbdc1a220cfcec9759e8197211d917e776644fb2
Nathan <nathanael.esnault@gmail.com>
2021-07-29 07:49:47 +1200

    Created testing files

---
+++ Test_Panic_Errors/Battery.h
@@ -0,0 +1,38 @@
+/* ========================================
+*
+* Copyright University of Auckland Ltd, 2021
+* All Rights Reserved
+* UNPUBLISHED, LICENSED SOFTWARE.
+*
+* Metadata
+* Written by    : Nathanaël Esnault
+* Verified by   : Nathanaël Esnault
+* Creation date : 2021-07-26
+* Version       : 0.1 (finished on 2021-..-..)
+* Modifications :
+* Known bugs    :
+*
+*
+* Possible Improvements
+*
+* Notes
+*
+*
+* Ressources (Boards + Libraries Manager)
+*
+*
+* TODO
+*
+* ========================================
+*/
+
+// -------------------------- Defines and Const --------------------------
+
+// Battery
+#define BATT_VOLTAGE_DIVIDER_FACTOR   2.0       // [N/A]
+#define LOW_BATTERY_THRESHOLD         3.1     // in [V] // <DEBUG> actual value should read 3.1
+#define BATT_PIN                      35      // To detect the Lipo battery remaining charge, GPIO35 on Adafruit ESP32 (35 on dev kit)
+
+
+
+// END OF THE FILE
